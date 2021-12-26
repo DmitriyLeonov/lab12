c4ffe766061858d526532eb0db07d448813499db
DmitriyLeonov <dleonov701@gmail.com>
2021-12-26 18:24:04 +0300

    Добавьте файлы проекта.

---
+++ lab12/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace lab12
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Exam exam = new Exam();
+            Reflector<Exam>.Asembly(exam.GetType());
+            Console.WriteLine();
+            Reflector<Exam>.PublicConstructors(exam.GetType());
+            Console.WriteLine();
+            Reflector<Exam>.PublicMethods(exam.GetType());
+            Reflector<Exam>.Interfaces(exam.GetType());
+            Reflector<Exam>.PublicMethods(exam.GetType());
+            Reflector<Exam>.Invoke(exam.GetType(), "GetDate", new object[] { });
+            Exam FirstExam = Reflector<Exam>.Create<Exam>(exam.GetType(), new Type[] { }, new object[] { });
+            Console.WriteLine(FirstExam.Theme);
+        }
+    }
+
+    abstract class Experiment
+    {
+        public abstract string GetTheme();
+
+        public virtual DateTime GetDate()
+        {
+            return DateTime.Now.AddDays(7);
+        }
+    }
+
+    class Test : Experiment, IQuestion
+    {
+        private string theme = "C#";
+        private int[] answers = new int[10] { 2, 4, 1, 1, 3, 1, 4, 4, 1, 3 };
+
+        public int CheckAnswer(int[] answers)
+        {
+            return 1;
+        }
+
+        string IQuestion.GetTheme()
+        {
+            return "Hello from interface";
+        }
+
+        public override string GetTheme()
+        {
+            return "Hello from class";
+        }
+
+        public override string ToString()
+        {
+            return theme;
+        }
+    }
+
+    class Exam : Experiment
+    {
+        public string Theme { get; set; }
+        private DateTime examDate = DateTime.Now.AddDays(12);
+
+        public Exam()
+        {
+            Theme = "OOP";
+            examDate = DateTime.Now.AddDays(12);
+        }
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return 1;
+        }
+
+        public override string GetTheme()
+        {
+            return this.Theme;
+        }
+
+        public override string ToString()
+        {
+            return "Theme";
+        }
+
+        public override DateTime GetDate()
+        {
+            return examDate;
+        }
+    }
+
+    sealed class FinalExam : Exam
+    {
+        public new string Theme { get; set; }
+        public override string ToString()
+        {
+            return this.Theme;
+        }
+    }
+
+    class Printer
+    {
+        public void IAmPrinting(Experiment obj)
+        {
+            Console.WriteLine(obj.GetType());
+            Console.WriteLine(obj.ToString());
+        }
+    }
+
+    public interface IQuestion
+    {
+        int CheckAnswer(int[] answers);
+        string GetTheme();
+    }
+}
---
+++ lab12/Reflector.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab12
+{
+    public static class Reflector<T>
+    {
+        public static void Asembly(Type t)
+        {
+            MemberInfo[] members = t.GetMembers();
+            string member = "";
+            foreach (MemberInfo m in members)
+            {
+                member += (m.Module.Assembly + "");
+            }
+            Console.WriteLine(member);
+            Console.WriteLine("----------------------------------------------------");
+            string path = @"vivod.txt";
+            using (StreamWriter sr = new StreamWriter(path, true, Encoding.Default))
+            {
+                sr.Write(member);
+            }
+        }
+
+        public static void PublicConstructors(Type t)
+        {
+            MemberInfo[] members = t.GetMembers();
+            string member = "";
+            foreach (MemberInfo m in members)
+            {
+                member += (m.ReflectedType.GetConstructors() + "");
+            }
+            if (member.Equals(""))
+            {
+                Console.WriteLine("есть");
+            }
+            else
+            {
+                Console.WriteLine("нет");
+            }
+            Console.WriteLine("----------------------------------------------------");
+            string path = @"vivod.txt";
+            using (StreamWriter sr = new StreamWriter(path, true, Encoding.Default))
+            {
+                sr.Write(member);
+            }
+        }
+
+        public static IEnumerable<string> PublicMethods(Type t)
+        {
+            MethodBase[] s = t.GetMethods();
+            IEnumerable<string> result = new List<string>();
+            Console.WriteLine("----------------------------------------------------");
+            foreach (MethodBase x in s)
+            {
+                if (x.IsPublic)
+                {
+                    Console.WriteLine(x.Name);
+                    result.Append(x.Name);
+                }
+            }
+            return result;
+        }
+
+        public static IEnumerable<string> Interfaces(Type t)
+        {
+            IEnumerable<string> result = new List<string>();
+            Console.WriteLine("----------------------------------------------------");
+            Type[] ty = t.GetInterfaces();
+            foreach (Type x in ty)
+            {
+                Console.WriteLine("----------------------------------------------------");
+                Console.WriteLine(x.Name);
+                result.Append(x.Name);
+            }
+            return result;
+        }
+
+        public static void Parametrs(Type t, Type pt)
+        {
+            MethodInfo[] s = t.GetMethods();
+            ParameterInfo[] p;
+            Console.WriteLine("----------------------------------------------------");
+            for (int i = 0; i < s.Length; i++)
+            {
+
+                p = s[i].GetParameters();
+                foreach (ParameterInfo j in p)
+                {
+
+                    if (j.ParameterType == pt)
+                    {
+
+                        Console.WriteLine(s[i].Name);
+                        break;
+                    }
+                }
+
+            }
+
+        }
+
+        public static void Invoke(Type t, string param, object[] paramArray)
+        {
+            Console.WriteLine("----------------------------------------------------");
+            MethodInfo method = t.GetMethod(param);
+            object obj = Activator.CreateInstance(t);
+            method.Invoke(obj, paramArray);
+        }
+
+        public static T Create<T>(Type type, Type[] parameters, object[] values)
+        {
+            ConstructorInfo info = type.GetConstructor(parameters);
+            object result = info.Invoke(values);
+            return (T)result;
+        }
+    }
+}
